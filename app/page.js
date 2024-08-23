46bb1f5f905de81c65af25f648938ad39ef0e136
imanj1470 <imanj1470@gmail.com>
2024-08-23 23:55:56 +0100

    homepage set

--- app/page.js
+++ app/page.js
@@ -1,29 +1,35 @@
+"use client"
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-
+import "/app/globals.css"
+import { useRouter } from "next/navigation"
 export default function HomePage() {
+    const router = useRouter()
   return (
     <Box
-      sx={{
+    sx={{
         width: '100vw',
         height: '100vh',
-        backgroundColor: '#F8F4FF',
+        backgroundImage: 'url(polyBackground.jpg)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         textAlign: 'center',
-        padding: 2,
+        padding: 0,
         margin: 0,
-        boxSizing: 'border-box',
-      }}
+        boxSizing: 'border-box'
+    }}
     >
       <Typography variant="h2" sx={{ mb: 4, color: '#333' }}>
         Welcome to Rate My Professor AI
       </Typography>
       <Box width="80vw">
-      <Typography variant="body1" sx={{ mb: 4, color: '#666' }}>
+      <Typography variant="body1" sx={{ mb: 4, color: '#505050' }}>
         Our AI-powered chatbot is here to help you with your questions and provide insights on professors nationwide. 
         Start a conversation and see how our chatbot can assist you today!
       </Typography>
@@ -36,7 +42,9 @@ export default function HomePage() {
             color: '#e0e0e0', // Lighter color for the text on hover
           },
         }}
-        //onClick={() => window.alert('Chatbot feature coming soon!')}
+        onClick={() => {
+            router.push('/chat')
+        }}
       >
         Start Chatting
       </Button>
